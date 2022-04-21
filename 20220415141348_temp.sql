99e0bbf099845404aaff7b1371a1978c83c66695
андрей плотников <forkpoons@gmail.com>
2022-04-21 10:57:00 +0700

    change migration

--- 20220415141348_temp.sql
+++ 20220415141348_temp.sql
@@ -9,8 +9,21 @@ CREATE TABLE temperatures
     PRIMARY KEY (id)
 );
 -- +goose StatementEnd
+-- +goose StatementBegin
+CREATE TABLE actions
+(
+    id     int AUTO_INCREMENT NOT NULL,
+    name   VARCHAR(30),
+    date   DATETIME,
+    action int,
+    PRIMARY KEY (id)
+);
+-- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 Drop TABLE temperatures;
+-- +goose StatementEnd
+-- +goose StatementBegin
+Drop TABLE actions;
 -- +goose StatementEnd
