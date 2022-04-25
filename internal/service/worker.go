0a52fe0d9165fb5c0758c80e48d8cde35bb6513a
forkpoons <forkpoons@gmail.com>
2022-04-25 12:25:36 +0700

    worker

--- internal/service/worker.go
+++ internal/service/worker.go
@@ -33,8 +33,9 @@ func New(addr string, repo repository) (*worker, error) {
 func (w *worker) Start() error {
 	conn = make(map[string]*websocket.Conn)
 	http.HandleFunc("/", w.get)
-	http.HandleFunc("/api/temp", w.postTemperature)
-	http.HandleFunc("/echo", w.echo)
+	http.HandleFunc("/api/temp", w.getTemperature)
+	http.HandleFunc("/api/settemp", w.postTemperature)
+	http.HandleFunc("/api/echo", w.echo)
 	return http.ListenAndServe(w.addr, nil)
 }
 
@@ -47,7 +48,7 @@ func (w *worker) get(rw http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func (w *worker) postTemperature(rw http.ResponseWriter, req *http.Request) {
+func (w *worker) getTemperature(rw http.ResponseWriter, req *http.Request) {
 	//if err := w.repo.WriteTemperature(1); err != nil {
 	//	вот так будешь вызывать потом
 	//}
@@ -67,6 +68,51 @@ func (w *worker) postTemperature(rw http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func (w *worker) postTemperature(rw http.ResponseWriter, req *http.Request) {
+	//if err := w.repo.WriteTemperature(1); err != nil {
+	//	вот так будешь вызывать потом
+	//}
+	body, err := ioutil.ReadAll(req.Body)
+	fmt.Println(string(body))
+	nn := 0
+	min := 0
+	max := 0
+	if body[0] == '#' && body[1] == '#' {
+		body = body[2:]
+		for n, m := range body {
+			//fmt.Println(nn, "/", n, "/", string(m))
+			if m == '|' {
+				min, err = strconv.Atoi(string(body[nn:n]))
+				nn = n + 1
+			} else if m == '#' {
+				max, err = strconv.Atoi(string(body[nn:n]))
+				break
+			}
+		}
+	}
+	rw.Header().Set("Content-Type", "application/json")
+	if err == nil {
+		for _, con := range conn {
+			err := con.WriteMessage(1, []byte("##"+strconv.Itoa(min)+"|"+strconv.Itoa(max)+"#"))
+			if err != nil {
+				log.Println(err)
+			}
+		}
+		_, err = rw.Write([]byte("ok"))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	} else {
+		log.Println(err)
+		_, err = rw.Write([]byte("error"))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
 var upgrader = websocket.Upgrader{} // use default options
 
 func (w *worker) echo(rw http.ResponseWriter, r *http.Request) {
@@ -80,45 +126,48 @@ func (w *worker) echo(rw http.ResponseWriter, r *http.Request) {
 
 	_, message, err := c.ReadMessage()
 	conn[string(message)] = c
+	defer delete(conn, string(message))
 	for {
 		_, message, err := c.ReadMessage()
 		if err != nil {
 			log.Println("read:", err)
 			break
 		}
-		msg := string(message)
-		msgValue := make([]string, 0)
-		nn := 0
-		fmt.Println(msg)
-		// ##id|typemsg|value#
-		if msg[0] == '#' && msg[1] == '#' {
-			msg = msg[2:]
-			for n, m := range msg {
-				//fmt.Println(nn, "/", n, "/", string(m))
-				if m == '|' {
-					msgValue = append(msgValue, msg[nn:n])
-					nn = n + 1
-				} else if m == '#' {
-					msgValue = append(msgValue, msg[nn:n])
-					break
+		if len(message) > 0 {
+			msg := string(message)
+			msgValue := make([]string, 0)
+			nn := 0
+			fmt.Println("123" + msg)
+			// ##id|typemsg|value#
+			if msg[0] == '#' && msg[1] == '#' {
+				msg = msg[2:]
+				for n, m := range msg {
+					//fmt.Println(nn, "/", n, "/", string(m))
+					if m == '|' {
+						msgValue = append(msgValue, msg[nn:n])
+						nn = n + 1
+					} else if m == '#' {
+						msgValue = append(msgValue, msg[nn:n])
+						break
+					}
 				}
-			}
-		}
-		fmt.Println(msgValue)
-		switch msgValue[1] {
-		case "temp":
-			value, err := strconv.ParseFloat(msgValue[2], 8)
-			if err != nil {
-				log.Println(err)
-				break
-			}
-			if err := w.repo.WriteTemperature(msgValue[0], int(value)); err != nil {
-				log.Println(err)
-			}
-		case "action":
-			fmt.Println("action")
-			if err := w.repo.WriteAction(msgValue[0], msgValue[2] == "1"); err != nil {
-				log.Println(err)
+				fmt.Println(msgValue)
+				switch msgValue[1] {
+				case "temp":
+					value, err := strconv.ParseFloat(msgValue[2], 8)
+					if err != nil {
+						log.Println(err)
+						break
+					}
+					if err := w.repo.WriteTemperature(msgValue[0], int(value)); err != nil {
+						log.Println(err)
+					}
+				case "action":
+					fmt.Println("action")
+					if err := w.repo.WriteAction(msgValue[0], msgValue[2] == "1"); err != nil {
+						log.Println(err)
+					}
+				}
 			}
 		}
 	}
