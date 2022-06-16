ab3603cb8e37d547803de001b16c3d98592aec14
андрей плотников <forkpoons@gmail.com>
2022-06-16 11:07:14 +0700

    refactoring

--- internal/database/repository.go
+++ internal/database/repository.go
@@ -3,7 +3,6 @@ package database
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
-	"time"
 )
 
 type Repository struct {
@@ -16,30 +15,4 @@ func New(database, conn string) (*Repository, error) {
 		return nil, err
 	}
 	return &Repository{Db: db}, nil
-}
-
-func (r *Repository) ReadTemperature(name string) (float64, error) {
-	var temp []float64
-	err := r.Db.Select(&temp, "SELECT temp FROM temperatures")
-	return temp[len(temp)-1], err
-}
-
-func (r *Repository) WriteTemperature(name string, temp float64) error {
-	_, err := r.Db.Exec(
-		"INSERT INTO temperatures (`name`, `date`, `temp`) VALUES(?,?,?)",
-		name,
-		time.Now().Add(time.Hour*7),
-		temp,
-	)
-	return err
-}
-
-func (r *Repository) WriteAction(name string, action bool) error {
-	_, err := r.Db.Exec(
-		"INSERT INTO actions (`name`, `date`, `action`) VALUES(?,?,?)",
-		name,
-		time.Now().Add(time.Hour*7),
-		action,
-	)
-	return err
 }
